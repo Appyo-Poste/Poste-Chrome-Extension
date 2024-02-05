b02106315c8038bd4b7bae7cdc1c67fdac892b2a
Kimberly Gehrmann <gehrmann.kimberly@gmail.com>
2024-02-04 20:26:45 -0800

    tsk 461; update webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,54 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    popup: path.join(__dirname, 'src', 'index.tsx'),
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      { test: /\.svg$/, use: ['file-loader'] },
+module.exports = (env) => {
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    mode: 'development',
+    entry: {
+      popup: path.join(__dirname, 'src', 'index.tsx'),
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        { test: /\.svg$/, use: ['file-loader'] },
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: path.resolve(__dirname, 'src/index.html'),
+        scriptLoading: 'module',
+        // filename
+        inject: true,
+      }),
+      new webpack.DefinePlugin(envKeys),
     ],
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    devServer: {
+      server: 'http',
+      historyApiFallback: true,
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+    },
+  };
 };
