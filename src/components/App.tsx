0dbb12d84a456f8aab088981bfad58ef51c10dc1
Kimberly Gehrmann <gehrmann.kimberly@gmail.com>
2024-04-14 08:38:51 -0700

    task 651

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,10 @@
-import React, { useContext, useState } from 'react';
-import { Route, MemoryRouter as Router, Routes } from 'react-router-dom';
+import React, { useContext, useState, useEffect } from 'react';
+import {
+  Route,
+  MemoryRouter as Router,
+  Routes,
+  useNavigate,
+} from 'react-router-dom';
 import { LoginForm } from './LoginForm';
 import { CreateUserForm } from './CreateUserForm';
 import { CreatePostForm } from './CreatePostForm';
@@ -9,8 +14,19 @@ import SuccessPage from './SuccessPage';
 import { AppContext } from './AppContext';
 
 const App = () => {
+  const navigate = useNavigate();
   const { isLoggedIn, setIsLoggedIn, token, setToken } = useContext(AppContext);
   const [error, setError] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    chrome.storage.local.get(['poste'], (result) => {
+      if (result && result.poste) {
+        setToken(result.poste);
+        setIsLoggedIn(true);
+        navigate('/post');
+      }
+    });
+  });
 
   return (
     <Router initialEntries={['/']}>
